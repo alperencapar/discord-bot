e25026ad22f5e622c11a44ebc59239c875bcb114
Alperen Çapar <alperencapar2@gmail.com>
2023-01-20 18:42:33 +0300

    FEATURE: delete command now supports specific user message deletion

--- src/commands/moderation/messageDelete.js
+++ src/commands/moderation/messageDelete.js
@@ -12,6 +12,12 @@ module.exports = {
 			description: "Number of message you want to delete",
 			type: ApplicationCommandOptionType.Integer,
 			required: true
+		},
+		{
+			name: "user",
+			description: "Owner of the messages that you want to delete",
+			type: ApplicationCommandOptionType.User,
+			required: false
 		}
 	],
 	permissionRequired: [PermissionFlagsBits.ManageMessages],
@@ -20,19 +26,53 @@ module.exports = {
 	callback: async (client, interaction) => {
 		const { channel, options } = interaction;
 		const messageCount = options.get("count").value;
+		const user = options.get("user")?.value;
 
-		/* 
-            const messages = await channel.messages.fetch({
-                limit: messageCount + 1
-            });
+		if (user) {
+			let userMessages = [];
+			const messages = await channel.messages.fetch({
+				limit: messageCount
+			});
 
-            const botMsg = await messages.find(
-                (msg) => msg.author.id === client.user.id
-            );
-            botMsg.delete();
-        */
+			let i = 1;
 
-		await channel.bulkDelete(messageCount + 1, true);
-		interaction.reply(`Silinen mesaj sayısı: ${messageCount}`);
+			while (userMessages.length < messageCount) {
+				if (i >= 10) break;
+
+				const messages = await channel.messages.fetch({
+					limit: messageCount * i
+				});
+
+				i++;
+				messages.map((msg) => {
+					if (msg.author.id === user & !(userMessages.includes(msg))) {
+						userMessages.length >= messageCount ? null : userMessages.push(msg)
+					}
+				});
+			}
+
+
+			if(userMessages.length > 0) {
+				channel.bulkDelete(userMessages).then(() => {
+					interaction.reply(
+						`Kullanıcının silinen mesaj sayısı: ${userMessages.length}`
+					)
+				})
+				
+			} else {
+				interaction.reply(
+					`Kullanıcıya ait mesaj bulunamadı`
+				);
+			}
+			
+		} else {
+			await channel.bulkDelete(messageCount + 1, true);
+			interaction.reply(`Silinen mesaj sayısı: ${messageCount}`);
+		}
+
+		// const botMsg = await messages.find(
+		//     (msg) => msg.author.id === client.user.id
+		// );
+		// botMsg.delete();
 	}
 };
