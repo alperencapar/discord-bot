56141d560c07c3cf37ee3fe86de01bf5b145662b
Alperen Çapar <alperencapar2@gmail.com>
2023-01-21 13:54:35 +0300

    CHANGE: poll file now has try-catch + variable naming changed + formatting

--- src/commands/moderation/poll.js
+++ src/commands/moderation/poll.js
@@ -1,5 +1,5 @@
 const {
-    EmbedBuilder,
+	EmbedBuilder,
 	ApplicationCommandOptionType,
 	PermissionFlagsBits
 } = require("discord.js");
@@ -16,56 +16,58 @@ module.exports = {
 		},
 		{
 			name: "channel",
-			description: "Channel that you want to display poll(if not provided, will published at the current channel)",
+			description:
+				"Channel that you want to display poll(if not provided, will published at the current channel)",
 			type: ApplicationCommandOptionType.Channel,
 			required: false
 		}
 	],
-	permissionRequired: [PermissionFlagsBits.ManageMessages],
-	botPermissions: [PermissionFlagsBits.ManageMessages],
+	permissionRequired: [PermissionFlagsBits.SendMessages],
+	botPermissions: [PermissionFlagsBits.EmbedLinks],
 
 	callback: async (client, interaction) => {
 		const { channel, options } = interaction;
 		const descriptionOfPoll = options.get("description").value;
 		const channelID = options.get("channel")?.value;
 
-        await interaction.deferReply({ ephemeral: true })
+		await interaction.deferReply({ ephemeral: true });
 
-        const userAvatar = interaction.user.displayAvatarURL({
-            format: "jpg",
-            size: 1024
-        });
+		try {
+			const userAvatar = interaction.user.displayAvatarURL({
+				format: "jpg",
+				size: 1024
+			});
 
-        let postChannel
-        if(channelID) {
-            postChannel = await interaction.guild.channels.fetch(channelID)
-        } else {
-            postChannel = channel
-        }
+			let pollChannel;
+			if (channelID) {
+				pollChannel = await interaction.guild.channels.fetch(channelID);
+			} else {
+				pollChannel = channel;
+			}
 
+			const pollEmbed = {
+				color: 0x0099ff,
+				title: "Anket!",
+				author: {
+					name: `${interaction.user.username}#${interaction.user.discriminator}`,
+					icon_url: userAvatar
+				},
+				description: descriptionOfPoll,
+				timestamp: new Date().toISOString(),
+				footer: {
+					icon_url: userAvatar
+				}
+			};
 
-        const pollEmbed = {
-            color: 0x0099ff,
-            title: 'Anket!',
-            author: {
-                name: `${interaction.user.username}#${interaction.user.discriminator}`,
-                icon_url: userAvatar,
-            },
-            description: descriptionOfPoll,
-            timestamp: new Date().toISOString(),
-            footer: {
-                icon_url: userAvatar,
-            },
-        };
-        
-
-        await interaction.editReply({
-            content: `Anket ${postChannel.toString()} kanalında oluşturuldu`,
-            ephemeral: true
-        })
-        const poll = await postChannel.send({embeds: [pollEmbed]})
-        await poll.react("✅")
-        await poll.react("❌")
-		
+			await interaction.editReply({
+				content: `Anket ${pollChannel.toString()} kanalında oluşturuldu`,
+				ephemeral: true
+			});
+			const poll = await pollChannel.send({ embeds: [pollEmbed] });
+			await poll.react("✅");
+			await poll.react("❌");
+		} catch (error) {
+			console.log(`Error at commands/misc/poll.js\n\nError: ${error}`);
+		}
 	}
 };
