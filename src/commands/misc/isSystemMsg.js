732a44430d80f4f93c7a712e9a51a7f20c8f3f68
Alperen Çapar <alperencapar2@gmail.com>
2023-04-19 20:39:48 +0300

    Feature: check message if it is system msg

---
+++ src/commands/misc/isSystemMsg.js
@@ -0,0 +1,41 @@
+const { ApplicationCommandOptionType } = require("discord.js")
+const errorFileLogHandler = require("../../handlers/errorFileLogHandler")
+
+module.exports = {
+	name: "check-msg",
+	description: "Check message if it is system message",
+	options: [
+		{
+			name: "message-id",
+			description: "Message id",
+			type: ApplicationCommandOptionType.String,
+			required: true,
+		},
+	],
+
+	callback: async (client, interaction) => {
+		try {
+			const messageId = interaction.options.get("message-id").value
+			const channel = interaction.channel
+
+			await interaction.channel.sendTyping()
+
+			const message = await channel.messages.fetch(messageId)
+
+			console.log(message)
+
+			const isSystem = await message.author.system
+
+			if (isSystem) {
+				interaction.reply(
+					`✅Mesaj doğrulandı! Mesaj sistem mesajı, güvenebilirsiniz!`
+				)
+			} else {
+				interaction.reply(`❌Sistem mesajı değil, güvenmeyin!❌`)
+			}
+		} catch (error) {
+			const ErrFileLocation = __dirname + __filename
+			errorFileLogHandler(error, ErrFileLocation, interaction)
+		}
+	},
+}
