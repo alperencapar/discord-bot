f7ab175c1ed25d9a7bbd1769a7a4efe6f4ef8f9e
Alperen Çapar <alperencapar2@gmail.com>
2023-04-27 16:08:26 +0300

    Feature: Chat Command Variable(ongoning)

--- src/events/messageCreate/test.js
+++ src/events/messageCreate/test.js
@@ -20,6 +20,9 @@ module.exports = async (client, message) => {
 		"chatCommand"
 	)
 
+	// const commandVariables = ["${user}", "${toUser}"]
+	const commandVariables = [{ user: "${user}" }, { toUser: "${toUser}" }]
+
 	for (const chatCommand of rec.chatCommand) {
 		let sendResponse = false
 		let commandAliases
@@ -44,12 +47,49 @@ module.exports = async (client, message) => {
 
 		if (sendResponse) {
 			let commandResponse = chatCommand.commandResponse
-			if (chatCommand.commandResponse.includes("${user}")) {
-				commandResponse = chatCommand.commandResponse.replace(
-					"${user}",
-					message.author.toString()
-				)
+
+			if (commandResponse.includes("${")) {
+				for (const variable of commandVariables) {
+					if (
+						chatCommand.commandResponse.includes(
+							variable[Object.keys(variable)]
+						)
+					) {
+						switch (variable[Object.keys(variable)]) {
+							case "${user}":
+								commandResponse = commandResponse.replace(
+									"${user}",
+									message.author.toString()
+								)
+
+								break
+							case "${toUser}":
+								const msgContentArr = message.content.split(" ")
+								const toUser = msgContentArr.find((txt) =>
+									txt.match(/<@[0-9]+>/g)
+								)
+								console.log(toUser)
+								commandResponse = commandResponse.replace(
+									"${toUser}",
+									toUser
+								)
+								console.log(commandResponse)
+
+								break
+
+							default:
+								break
+						}
+					}
+				}
 			}
+
+			// if (chatCommand.commandResponse.includes("${user}")) {
+			// 	commandResponse = chatCommand.commandResponse.replace(
+			// 		"${user}",
+			// 		message.author.toString()
+			// 	)
+			// }
 			message.reply(`${commandResponse}`)
 		}
 	}
